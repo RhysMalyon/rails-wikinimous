d380d38ca723df497f554e10dbdc2586774135d7
Rhys Malyon <malyonr@gmail.com>
2021-08-10 17:02:15 +0900

    Complete project

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,41 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show update edit destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show; end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+
+  def edit; end
+
+  def update
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+end
